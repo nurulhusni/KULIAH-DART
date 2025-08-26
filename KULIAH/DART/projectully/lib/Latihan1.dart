80e87277a38f8562a6157654dcc1b7b3aebcffd5
nurulhusni <ulihusen@gmail.com>
2025-08-26 13:10:13 +0900

    Upload Latihan1

---
+++ KULIAH/DART/projectully/lib/Latihan1.dart
@@ -0,0 +1,13 @@
+void main() {
+    String name = "Nurul Husni";
+    int umur = 20;
+    double berat_badan = 60;
+    num Tinggi_badan = 155;
+    dynamic prodi = "informatika";
+
+    print("name : $name");
+    print(umur.toString().runtimeType);
+    print(berat_badan);
+    print(Tinggi_badan);
+    print("prodi : $prodi");
+}
---
+++ KULIAH/DART/projectully/lib/set.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  Set<int> angka = {1, 2, 3, 4, 5};
+
+  print(angka.length);
+  print(angka.add(6));
+  print(angka);
+}
